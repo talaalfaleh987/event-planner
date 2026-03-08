43b5a50b254a88df9134a848e92694340b2e53a2
Tala <talaalfaleh987@gmail.com>
2026-03-08 19:50:30 +0300

    feat: header

---
+++ eventManagement/src/app/enums/typography.enum.ts
@@ -0,0 +1,8 @@
+export enum TypographyStyle {
+  TITLE1 = 'text-[26px] text-primary font-medium',
+  TITLE2 = 'text-[18px] text-primary font-medium',
+  TITLE3 = 'text-[16px] text-primary font-medium',
+  TITLE4 = 'text-[14px] text-primary font-normal',
+  TITLE5 = 'text-[12px] text-subtitle font-regular',
+  SUBTITLE = 'text-[16px] text-subtitle font-medium',
+}
