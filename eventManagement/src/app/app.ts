837174b4a22e9693cb760b1d2405d36e0fd952a6
Tala <talaalfaleh987@gmail.com>
2026-03-08 19:50:30 +0300

    feat: header
    ^ Conflicts:
    ^	eventManagement/src/app/app.ts

--- eventManagement/src/app/app.ts
+++ eventManagement/src/app/app.ts
@@ -1,14 +1,17 @@
-import { Component, inject, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, input, signal } from '@angular/core';
+import { NavItem } from './models/nav-item';
+import { Header } from './components/header/header';
+import { TypographyStyle } from './enums/typography.enum';
+import { Text } from './components/text/text';
 import { TranslateButton } from './components/translate-button/translate-button';
 import { Language } from './enums/language.enum';
 import { TranslatePipe, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet ,TranslatePipe],
+  imports: [Header, Text ,TranslatePipe],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('eventManagement');
@@ -19,4 +22,11 @@ export class App {
     this.translate.setFallbackLang(Language.ARABIC);
     this.translate.use(Language.ARABIC);
   }
+
+  readonly TypographyStyle = TypographyStyle;
+
+  navItems: NavItem[] = [
+    { label: 'الرئيسية', route: '/home' },
+    { label: 'إدارة الفعاليات', route: '/events' },
+  ];
 }
