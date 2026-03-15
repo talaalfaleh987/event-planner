6c43b6b13f6f2ced2da5893d9473b95b3810a1be
lamyax66-66 <lamyax66@gmail.com>
2026-03-15 20:37:53 +0300

    fix: login UI

--- eventManagement/src/app/components/input/input.ts
+++ eventManagement/src/app/components/input/input.ts
@@ -1,4 +1,4 @@
-import { Component, computed, input } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { InputType, ValidatorType } from '../../enums/input.enum';
 import { InputErrorMessage } from '../../models/input-error-message';
@@ -12,21 +12,16 @@ export class CustomInput {
   control = input.required<FormControl>();
   inputType = input<InputType>(InputType.TEXT);
   label = input<string>('');
-  placeholder = input<string>('');
   errors = input<InputErrorMessage[]>([]);
 
-  isCheckbox = computed(() => this.inputType() === InputType.CHECKBOX);
-
   get hasError(): boolean {
-    const control = this.control();
-    if (!control?.errors) return false;
-    return !!control.touched;
-  }
+  const control = this.control();
+  return !!(control && control.invalid && control.touched);
+}
 
   hasErrorByType(validatorTypes: ValidatorType[]): boolean {
     const control = this.control();
     if (!control?.errors) return false;
-
     for (const validatorType of validatorTypes) {
       if (control.errors?.[validatorType]) {
         return this.hasError;
--- eventManagement/src/app/enums/message.enum.ts
+++ eventManagement/src/app/enums/message.enum.ts
@@ -1,4 +1,4 @@
 export enum MessageStyle {
-  MESSAGE = 'w-full rounded-md border-2 font-semibold flex font-neo-medium items-center justify-center p-1',
+  MESSAGE = 'w-full rounded-md border-2 font-semibold flex font-neo-medium p-1',
   ERROR = MESSAGE + ' border-red-error text-red-error bg-errorBg-normal',
 }
