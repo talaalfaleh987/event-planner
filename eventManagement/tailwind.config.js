574b31500fa98b3bd5073635c8555671e7210302
Tala <talaalfaleh987@gmail.com>
2026-03-15 12:55:50 +0300

    fix: footer

--- eventManagement/tailwind.config.js
+++ eventManagement/tailwind.config.js
@@ -4,10 +4,36 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: '#264653',
-        subtitle: '#707070',
-        error: '#9D1C1C',
-        errorBg: '#F3E3E3',
+        primary: {
+          normal: '#264653',
+          100: '',
+          200: '',
+          300: '',
+          400: '',
+          500: '',
+        },
+        grey: {
+          subtitle: '#707070',
+          100: '#F9F9F9',
+          200: '',
+          300: '',
+          400: '',
+          500: '',
+        },
+        red: {
+          error: '#9D1C1CCC',
+          100: '',
+          200: '',
+          300: '',
+        },
+        errorBg: {
+          normal: '#F3E3E3',
+          100: '',
+          200: '',
+        },
+      },
+      backgroundImage: {
+        'main-gradient': 'linear-gradient(112.9deg, #1D3557 0%, #264653 50%, #5FB6AB 100%)',
       },
     },
   },
