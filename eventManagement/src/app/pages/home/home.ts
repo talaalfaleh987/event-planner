af6278ac7a49070b56fef17a16a200a0e509eba5
Tala <talaalfaleh987@gmail.com>
2026-03-30 10:40:00 +0300

    feat: stack bar chart ui

--- eventManagement/src/app/pages/home/home.ts
+++ eventManagement/src/app/pages/home/home.ts
@@ -1,50 +1,44 @@
 import { Component, computed, inject } from '@angular/core';
 import { StackedBarChart } from '../../components/charts/bar/stacked-bar-chart/stacked-bar-chart';
 import { MonthlyEventsChartItem } from '../../models/charts/monthly-events-chart-Item';
-import { TranslateService } from '@ngx-translate/core';
+import { TranslatePipe, TranslateService } from '@ngx-translate/core';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { ChartData } from '../../models/charts/chart-data';
+import { CardStyle } from '../../enums/card.enum';
+import { Card } from '../../components/card/card';
+import { EventService } from '../../service/events/event-service';
 
 @Component({
   selector: 'app-home',
-  imports: [StackedBarChart],
+  imports: [StackedBarChart, Card, TranslatePipe],
   templateUrl: './home.html',
 })
 export class Home {
   private readonly translate = inject(TranslateService);
 
   private readonly currentLang = toSignal(this.translate.onLangChange);
+  protected readonly CardStyle = CardStyle;
+  private readonly eventService = inject(EventService);
 
-  protected monthlyEventsData: MonthlyEventsChartItem[] = [
-    { month: '1', physical: 5000, remote: 1500 },
-    { month: '2', physical: 10500, remote: 5000 },
-    { month: '3', physical: 7200, remote: 3000 },
-    { month: '4', physical: 14000, remote: 7000 },
-    { month: '5', physical: 2500, remote: 800 },
-    { month: '6', physical: 8000, remote: 3500 },
-    { month: '7', physical: 10500, remote: 5000 },
-    { month: '8', physical: 5000, remote: 1500 },
-    { month: '9', physical: 15500, remote: 7800 },
-    { month: '10', physical: 6800, remote: 2800 },
-    { month: '11', physical: 8500, remote: 3700 },
-    { month: '12', physical: 5000, remote: 1500 },
-  ];
+  protected monthlyEventsData = toSignal(this.eventService.getMonthlyEventsData(), {
+    initialValue: [],
+  });
 
   protected chartData = computed<ChartData>(() => {
     const lang = this.currentLang();
 
     return {
-      labels: this.monthlyEventsData.map((item: MonthlyEventsChartItem) => item.month),
+      labels: this.monthlyEventsData().map((item: MonthlyEventsChartItem) => item.month),
       series: [
         {
           name: this.translate.instant('EVENTS.REMOTE'),
-          data: this.monthlyEventsData.map((item: MonthlyEventsChartItem) => item.remote),
+          data: this.monthlyEventsData().map((item: MonthlyEventsChartItem) => item.remote),
           color: '#62B7AE',
           borderRadius: [0, 0, 6, 6],
         },
         {
           name: this.translate.instant('EVENTS.PHYSICAL'),
-          data: this.monthlyEventsData.map((item: MonthlyEventsChartItem) => item.physical),
+          data: this.monthlyEventsData().map((item: MonthlyEventsChartItem) => item.physical),
           color: '#B14696',
           borderRadius: [6, 6, 0, 0],
         },
--- eventManagement/src/app/service/events/event-service.ts
+++ eventManagement/src/app/service/events/event-service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { of, Observable } from 'rxjs';
 import { Event } from '../../models/event';
+import { MonthlyEventsChartItem } from '../../models/charts/monthly-events-chart-Item';
 
 @Injectable({
   providedIn: 'root',
@@ -8,7 +9,7 @@ import { Event } from '../../models/event';
 export class EventService {
   getEvent(): Observable<Event> {
     return of({
-       name:'مؤتمر التقنية 2026',
+      name: 'مؤتمر التقنية 2026',
       category: 'Work',
       type: 'online',
       location: 'Virtual',
@@ -19,5 +20,20 @@ export class EventService {
     });
   }
 
-   
+  getMonthlyEventsData(): Observable<MonthlyEventsChartItem[]> {
+    return of([
+      { month: '1', physical: 5000, remote: 1500 },
+      { month: '2', physical: 10500, remote: 5000 },
+      { month: '3', physical: 7200, remote: 3000 },
+      { month: '4', physical: 14000, remote: 7000 },
+      { month: '5', physical: 2500, remote: 800 },
+      { month: '6', physical: 8000, remote: 3500 },
+      { month: '7', physical: 10500, remote: 5000 },
+      { month: '8', physical: 5000, remote: 1500 },
+      { month: '9', physical: 15500, remote: 7800 },
+      { month: '10', physical: 6800, remote: 2800 },
+      { month: '11', physical: 8500, remote: 3700 },
+      { month: '12', physical: 5000, remote: 1500 },
+    ]);
+  }
 }
