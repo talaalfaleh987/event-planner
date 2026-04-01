d802d483d9810a98271cf53e214ff58a3675b168
Tala <talaalfaleh987@gmail.com>
2026-04-01 13:15:43 +0300

    fix: stacked barchart

--- eventManagement/src/app/components/charts/bar/stacked-bar-chart/stacked-bar-chart.ts
+++ eventManagement/src/app/components/charts/bar/stacked-bar-chart/stacked-bar-chart.ts
@@ -1,4 +1,4 @@
-import { Component, computed, input } from '@angular/core';
+import { Component, computed, inject, input } from '@angular/core';
 import { NgxEchartsDirective, provideEchartsCore } from 'ngx-echarts';
 import * as echarts from 'echarts/core';
 import { BarChart } from 'echarts/charts';
@@ -6,6 +6,9 @@ import { GridComponent, LegendComponent, TooltipComponent } from 'echarts/compon
 import { CanvasRenderer } from 'echarts/renderers';
 import { stackedBarChartConfig } from './stacked-bar-chart.config';
 import { ChartData } from '../../../../models/charts/chart-data';
+import { MonthlyEventsChartItem } from '../../../../models/charts/monthly-events-chart-Item';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { TranslateService } from '@ngx-translate/core';
 
 echarts.use([BarChart, GridComponent, LegendComponent, TooltipComponent, CanvasRenderer]);
 
@@ -16,7 +19,32 @@ echarts.use([BarChart, GridComponent, LegendComponent, TooltipComponent, CanvasR
   templateUrl: './stacked-bar-chart.html',
 })
 export class StackedBarChart {
-  data = input.required<ChartData>();
+  // data = input.required<ChartData>();
+  data = input.required<MonthlyEventsChartItem[]>();
+  private readonly translate = inject(TranslateService);
+  private readonly currentLang = toSignal(this.translate.onLangChange);
 
-  chartOptions = computed(() => stackedBarChartConfig(this.data()));
+  private readonly chartData = computed<ChartData>(() => {
+    this.currentLang();
+
+    return {
+      labels: this.data().map((item: MonthlyEventsChartItem) => item.month),
+      series: [
+        {
+          name: this.translate.instant('EVENTS.REMOTE'),
+          data: this.data().map((item: MonthlyEventsChartItem) => item.remote),
+          color: '#62B7AE',
+          borderRadius: [0, 0, 6, 6],
+        },
+        {
+          name: this.translate.instant('EVENTS.PHYSICAL'),
+          data: this.data().map((item: MonthlyEventsChartItem) => item.physical),
+          color: '#B14696',
+          borderRadius: [6, 6, 0, 0],
+        },
+      ],
+    };
+  });
+
+  chartOptions = computed(() => stackedBarChartConfig(this.chartData()));
 }
--- eventManagement/src/app/pages/home/home.ts
+++ eventManagement/src/app/pages/home/home.ts
@@ -1,9 +1,7 @@
-import { Component, computed, inject } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { StackedBarChart } from '../../components/charts/bar/stacked-bar-chart/stacked-bar-chart';
-import { MonthlyEventsChartItem } from '../../models/charts/monthly-events-chart-Item';
-import { TranslatePipe, TranslateService } from '@ngx-translate/core';
+import { TranslatePipe } from '@ngx-translate/core';
 import { toSignal } from '@angular/core/rxjs-interop';
-import { ChartData } from '../../models/charts/chart-data';
 import { CardStyle } from '../../enums/card.enum';
 import { Card } from '../../components/card/card';
 import { EventService } from '../../service/events/event-service';
@@ -14,34 +12,10 @@ import { EventService } from '../../service/events/event-service';
   templateUrl: './home.html',
 })
 export class Home {
-  private readonly translate = inject(TranslateService);
   private readonly eventService = inject(EventService);
-  private readonly currentLang = toSignal(this.translate.onLangChange);
   protected readonly CardStyle = CardStyle;
 
   protected monthlyEventsData = toSignal(this.eventService.getMonthlyEventsData(), {
     initialValue: [],
-  });
-
-  protected chartData = computed<ChartData>(() => {
-    const lang = this.currentLang();
-
-    return {
-      labels: this.monthlyEventsData().map((item: MonthlyEventsChartItem) => item.month),
-      series: [
-        {
-          name: this.translate.instant('EVENTS.REMOTE'),
-          data: this.monthlyEventsData().map((item: MonthlyEventsChartItem) => item.remote),
-          color: '#62B7AE',
-          borderRadius: [0, 0, 6, 6],
-        },
-        {
-          name: this.translate.instant('EVENTS.PHYSICAL'),
-          data: this.monthlyEventsData().map((item: MonthlyEventsChartItem) => item.physical),
-          color: '#B14696',
-          borderRadius: [6, 6, 0, 0],
-        },
-      ],
-    };
   });
 }
