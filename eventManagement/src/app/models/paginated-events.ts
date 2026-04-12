24c187576f520e31fcf0265bc34ef9bfe3a54f41
lamyax66-66 <lamyax66@gmail.com>
2026-04-12 11:23:07 +0300

    feat: paginator
    ^ Conflicts:
    ^	eventManagement/package-lock.json
    ^	eventManagement/src/app/service/events/event-service.ts
    ^	eventManagement/tailwind.config.js

---
+++ eventManagement/src/app/models/paginated-events.ts
@@ -0,0 +1,11 @@
+import { EventData } from "./data";
+
+export interface PaginatedEvents {
+  items: EventData[];
+  totalItems: number;
+  totalPages: number;
+  currentPage: number;
+  pageSize: number;
+  hasPrevious: boolean;
+  hasNext: boolean;
+}
